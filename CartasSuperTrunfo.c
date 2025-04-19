dbf144cf4189b8233664829da19c021d07173427
Rodrigo Lamego <rglamego@outlook.com>
2025-04-19 11:47:20 -0300

    Inicio do nuvel aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <locale.h>
 
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
@@ -7,86 +8,78 @@
 //Teste larissa
 
 int main() {
-    char codigoCarta1[3], codigoCarta2[3];
-    char Estado1, Estado2; // Código da cidade
-    char nomeCidade1[50], nomeCidade2[50]; // Nome da cidade  
-    int populacao1, populacao2; // População da cidade
-    float area1, area2; // Área da cidade               
-    float pib1, pib2; // PIB da cidade
-    int numPontosTuristicos1, numPontosTuristicos2; // Número de pontos turísticos da cidade
+    setlocale(LC_ALL, "pt_BR.UTF-8"); // Configura o locale para português
+    // Definição das variáveis
+    char codigoCarta1[4], codigoCarta2[4],
+    Estado1, Estado2, nomeCidade1[50], nomeCidade2[50]; // Nome da cidade  
+    int populacao1, populacao2,numPontosTuristicos1, numPontosTuristicos2; // População da cidade
+    float area1, area2, pib1, pib2; // PIB da cidade
+    
 
     printf("Cadastro de Cartas - Cidades\n");
     printf("Carta 1\n");
     
     printf("Digite a letra do Estado: ");      
-    scanf("%c", &Estado1);
-    getchar(); // Consome o '\n' residual
+    scanf(" %c", &Estado1);
+    
     
     printf("Digite o código da carta: ");
-    scanf("%s", codigoCarta1);
-    getchar(); // Consome o '\n' residual
+    scanf(" %3s", codigoCarta1);
+    
     
     printf("Digite o nome da cidade: ");
-    scanf("%49[^\n]", nomeCidade1);
-    getchar(); // Consome o '\n' residual
+    scanf(" %49[^\n]", nomeCidade1);
+    
     
     printf("Digite a população da cidade: ");
-    scanf("%i", &populacao1);
-    getchar(); // Consome o '\n' residual
+    scanf("%d", &populacao1);
+    
     
     printf("Digite a área da cidade: ");
     scanf("%f", &area1);
-    getchar(); // Consome o '\n' residual
+    while (getchar() != '\n');   
+    
     
     printf("Digite o PIB da cidade: ");
-    scanf("%f", &pib1);    
-    getchar();
+    scanf("%f", &pib1);
+    // while (getchar() != '\n');    // tive que adicionar isso para limpar o buffer do teclado, pois 
+    // ao digitar uma número float ex. 2.200 estava causando problemas na leitura do próximo scanf
+    
+    
     printf("Digite o número de pontos turísticos da cidade: ");
-    scanf("%i", &numPontosTuristicos1);    
-    getchar();
+    scanf("%i", &numPontosTuristicos1);  
+    
 // dados da segunda carta
 
 printf("Cadastro de Cartas - Cidades\n");
     printf("Carta 2\n");
     
     printf("Digite a letra do Estado: ");      
-    scanf(" %c", &Estado2); // Corrigido para %c e sem espaços extras
-    getchar();
+    scanf(" %c", &Estado2); 
+    
 
     printf("Digite o código da carta: ");
-    scanf("%s", codigoCarta2); // Corrigido para %s sem espaços extras
-    getchar();
+    scanf("%s", codigoCarta2); 
+    
 
     printf("Digite o nome da cidade: ");
     scanf(" %49[^\n]", nomeCidade2); // Lê até 49 caracteres ou até encontrar uma nova linha
-    getchar();
+    
 
     printf("Digite a população da cidade: ");
-    scanf("%i", &populacao2);      
-    getchar();
+    scanf("%d", &populacao2);      
+    
 
     printf("Digite a área da cidade: ");
-    scanf("%f", &area2);   
-    getchar();
+    scanf(" %f", &area2);   
+    
 
     printf("Digite o PIB da cidade: ");
-    scanf("%f", &pib2);    
-    getchar();
+    scanf(" %f", &pib2);    
     
+
     printf("Digite o número de pontos turísticos da cidade: ");
-    scanf("%i", &numPontosTuristicos2);
-
-
-
-
-
-
-
-
-
-
-
-
+    scanf(" %d", &numPontosTuristicos2);
 
     // Exibição dos dados cadastrados
     printf("\nCarta 1:\n");
